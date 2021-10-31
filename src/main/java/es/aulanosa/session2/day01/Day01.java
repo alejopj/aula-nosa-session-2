8dae85367873c365277d0b45f2fe9f53338fc6a2
alejopj <alejopj@gmail.com>
2021-10-31 19:24:03 +0100

    Add exercise solutions

--- src/main/java/es/aulanosa/session2/day01/Day01.java
+++ src/main/java/es/aulanosa/session2/day01/Day01.java
@@ -12,13 +12,34 @@ public class Day01 {
 	
 	public List<Integer> findTwoNumbersSum2020(List<Integer> numbers) {
 		
-		// TODO Your code goes here.
+		for (int i = 0; i < numbers.size() - 1; i++) {
+			for (int j = i + 1; j < numbers.size(); j++) {
+				Integer a = numbers.get(i);
+				Integer b = numbers.get(j);
+				if (a + b == 2020) {
+					return ImmutableList.of(a, b);
+				}
+			}
+		}
+		
 		return ImmutableList.of();
 	}
 	
 	public List<Integer> findThreeNumbersSum2020(List<Integer> numbers) {
 		
-		// TODO Your code goes here.
+		for (int i = 0; i < numbers.size() - 2; i++) {
+			for (int j = i + 1; j < numbers.size() - 1; j++) {
+				for (int k = j + 1; k < numbers.size(); k++) {
+					Integer a = numbers.get(i);
+					Integer b = numbers.get(j);
+					Integer c = numbers.get(k);
+					if (a + b + c == 2020) {
+						return ImmutableList.of(a, b, c);
+					}
+				}
+			}
+		}
+		
 		return ImmutableList.of();
 	}
 }
--- src/main/java/es/aulanosa/session2/day02/Day02.java
+++ src/main/java/es/aulanosa/session2/day02/Day02.java
@@ -11,14 +11,40 @@ public class Day02 {
 	
 	public Integer findNumberOfValidPasswords(Map<PasswordPolicy, Collection<String>> passwordsByPolicy) {
 		
-		// TODO Your code goes here.
-		return null;
+		return passwordsByPolicy.entrySet().parallelStream().mapToInt(entry ->
+				findNumberOfValidPasswords(entry.getKey(), entry.getValue())).sum();
 	}
 	
 	public Integer findNewNumberOfValidPasswords(Map<PasswordPolicy, Collection<String>> passwordsByPolicy) {
 		
-		// TODO Your code goes here.
-				return null;
+		return passwordsByPolicy.entrySet().parallelStream().mapToInt(entry ->
+				findNewNumberOfValidPasswords(entry.getKey(), entry.getValue())).sum();
 	}
 	
+	// Private
+
+	private Integer findNumberOfValidPasswords(PasswordPolicy policy, Collection<String> collection) {
+		
+		Integer min = policy.getMin();
+		Integer max = policy.getMax();
+		String character = policy.getCharacter();
+		return (int) collection.parallelStream().filter(password -> {
+				String p = password;
+				int appearances = 0;
+				while (p.contains(character)) {
+					p = p.replaceFirst(character, "");
+					appearances++;
+				}
+				return appearances >= min && appearances <= max;
+			}).count();
+	}
+	
+	private Integer findNewNumberOfValidPasswords(PasswordPolicy policy, Collection<String> collection) {
+		
+		Integer min = policy.getMin() - 1;
+		Integer max = policy.getMax() - 1;
+		String character = policy.getCharacter();
+		return (int) collection.parallelStream().filter(password ->
+				String.valueOf(password.charAt(min)).equals(character) ^ String.valueOf(password.charAt(max)).equals(character)).count();
+	}
 }
